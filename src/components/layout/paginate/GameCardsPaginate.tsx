dd6ecc07e1a1d67c44357674a9074d580fc4ccfa
Tomás Serra <tomi.serra@gmail.com>
2024-12-10 14:48:52 -0300

    Fixed order by

--- src/components/layout/paginate/GameCardsPaginate.tsx
+++ src/components/layout/paginate/GameCardsPaginate.tsx
@@ -6,7 +6,7 @@ import Dropdown from "@/components/ui/dropdown/Dropdown";
 import { CiFilter } from "react-icons/ci";
 import { getGameCardsInfo } from "@/utils/DataMapper";
 import { GameCardInfo } from "@/types/gameCardInfo";
-import { OrderByOptions } from "@/types/orderByOptions";
+import { OrderByOptions } from "@/types/ordering";
 import Filters from "@/components/utils/filters/Filters";
 import { useFilters } from "@/contexts/filters/FiltersContext";
 import { GameFilters } from "@/types/filters";
